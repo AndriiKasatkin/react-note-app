c2e79af4dea31bb01f9843d3cee5cd0be5323c69
Andrii Kasatkin <kasatkin1998andriy@gmail.com>
2021-01-23 19:01:37 +0200

    Add firebase db and connection, functionality and some animation

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,19 +1,35 @@
 import React, { useContext } from 'react'
+import { CSSTransition } from 'react-transition-group'
 import { AlertContext } from '../context/alert/alertContext'
 
 export const Alert = () => {
   const { alert, hide } = useContext(AlertContext)
 
-  if (!alert.visible) {
-    return null
-  }
   return (
-    <div class={`alert alert-${alert.type || 'warning'} alert-dismissible`}>
-      <strong>Attention!</strong>
-      {alert.text}
-      <button onClick={hide} type="button" className="close" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
+    <CSSTransition
+      in={alert.visible}
+      timeout={{
+        enter: 500,
+        exit: 350,
+      }}
+      className={'alert'}
+      mountOnEnter
+      unmountOnExit
+    >
+      <div
+        className={`alert alert-${alert.type || 'warning'} alert-dismissible`}
+      >
+        <strong>Attention!</strong>
+        &nbsp;{alert.text}
+        <button
+          onClick={hide}
+          type="button"
+          className="close"
+          aria-label="Close"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    </CSSTransition>
   )
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,24 @@
 import React, { useState, useContext } from 'react'
 import { AlertContext } from '../context/alert/alertContext'
+import { FirebaseContext } from '../context/firebase/firebaseContext'
 
 export const Form = () => {
   const [value, setValue] = useState('')
   const alert = useContext(AlertContext)
+  const firebase = useContext(FirebaseContext)
 
   const submitHandler = (event) => {
     event.preventDefault()
 
     if (value.trim()) {
-      alert.show('Note has been created', 'success')
+      firebase
+        .addNote(value.trim())
+        .then(() => {
+          alert.show('Note has been created', 'success')
+        })
+        .catch(() => {
+          alert.show('Some error', 'danger')
+        })
       setValue('')
     } else {
       alert.show('Enter the text')
